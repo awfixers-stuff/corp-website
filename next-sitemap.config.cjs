cfe6b69031cf6e5cd59c648261bca30314b94160
awfixer <git@awfixer.me>
2026-04-16 21:18:00 -0700

    create corp-website

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: process.env.SITEMAP_URL || 'https://corp.awfixer.me',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
---
+++ src/app/(payload)/api/graphql-playground/route.ts
@@ -0,0 +1,7 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import config from '@payload-config'
+import '@awfixers-stuff/next/css'
+import { GRAPHQL_PLAYGROUND_GET } from '@awfixers-stuff/next/routes'
+
+export const GET = GRAPHQL_PLAYGROUND_GET(config)
