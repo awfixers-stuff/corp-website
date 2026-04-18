51e0d8f3e1bf4d5f1db8f4123264c1618ef69688
awfixer <git@awfixer.me>
2026-04-17 20:10:43 -0700

    feat: update corpsite

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241116_194708_migration from './20241116_194708_migration'
-
-export const migrations = [
-  {
-    name: '20241116_194708_migration',
-    down: migration_20241116_194708_migration.down,
-    up: migration_20241116_194708_migration.up,
-  },
-]
